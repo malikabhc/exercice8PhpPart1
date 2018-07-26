eb9a4ce964c6fcebc6173471d0ca5058a7b58e48
Malikabhc <malika.bouhcine@gmail.com>
2018-07-26 13:49:12 +0200

    exercice8

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <?php
+      $first = 3 + 4;
+      $second = 5 * 20;
+      $third = 45 / 5;
+      echo $first . ' ' . $second . ' ' . $third;
+    ?>
+  </body>
+</html>
